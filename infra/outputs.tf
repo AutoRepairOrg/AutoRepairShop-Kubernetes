e38070ca3e758b4cdc30f9a4f53b07af11b4fc63
dhiulia <85645860+dhiuliaantunes@users.noreply.github.com>
2026-08-18 11:51:23 -0300

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,9 +1,19 @@
-output "namespace" {
-  description = "Kubernetes namespace"
-  value       = kubernetes_namespace.oficina.metadata[0].name
+output "eks_cluster_name" {
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.autorepairshop.name
+}
+
+output "eks_cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = aws_eks_cluster.autorepairshop.endpoint
+}
+
+output "eks_node_group" {
+  description = "EKS node group"
+  value       = aws_eks_node_group.autorepairshop.node_group_name
 }
 
 output "environment" {
-  description = "Deployed environment"
+  description = "Environment"
   value       = var.environment
 }

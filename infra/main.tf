aaf39ec67cc65a4c368ead06bc5e337c656bf041
dhiulia <85645860+dhiuliaantunes@users.noreply.github.com>
2026-08-18 11:50:18 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,42 @@
-resource "kubectl_manifest" "k8s" {
-  for_each  = fileset("${path.module}/../k8s", "*.yaml")
-  yaml_body = file("${path.module}/../k8s/${each.value}")
+data "aws_iam_role" "lab" {
+  name = "LabRole"
+}
+
+resource "aws_eks_cluster" "autorepairshop" {
+  name     = "autorepairshop-eks"
+  role_arn = data.aws_iam_role.lab.arn
+  version  = "1.31"
+
+  vpc_config {
+    subnet_ids = [
+      "subnet-07d988ad8c5e3a697",
+      "subnet-0fc6ceff22ca1c5eb"
+    ]
+
+    endpoint_public_access  = true
+    endpoint_private_access = false
+  }
+}
+
+resource "aws_eks_node_group" "autorepairshop" {
+  cluster_name    = aws_eks_cluster.autorepairshop.name
+  node_group_name = "autorepairshop-nodes"
+  node_role_arn   = data.aws_iam_role.lab.arn
+
+  subnet_ids = [
+    "subnet-07d988ad8c5e3a697",
+    "subnet-0fc6ceff22ca1c5eb"
+  ]
+
+  instance_types = ["t3.small"]
+
+  scaling_config {
+    desired_size = 1
+    min_size     = 1
+    max_size     = 2
+  }
 
   depends_on = [
-    kubernetes_namespace.oficina
+    aws_eks_cluster.autorepairshop
   ]
 }

469305ec877e1862e8588d6b0317eb70ffcaf468
dhiulia <85645860+dhiuliaantunes@users.noreply.github.com>
2026-08-18 11:50:47 -0300

    Update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,14 +1,24 @@
 terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.25"
     }
+
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = "~> 1.14"
     }
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
 
 provider "kubernetes" {
